ce7e98c354b26223bf8777f6f41387b40f67fd2c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-13 15:31:38 -0500

    111. Separar las rutas y el controlador de la clase

---
+++ controllers/usuarios.js
@@ -0,0 +1,38 @@
+const { response } = require('express');
+
+const usuariosGet = (req, res = response) => {
+	res.json({
+		msg: 'get API - controlador'
+	});
+};
+
+const usuariosPost = (req, res = response) => {
+	res.json({
+		msg: 'post API - usuariosPost'
+	});
+};
+
+const usuariosPut = (req, res = response) => {
+	res.json({
+		msg: 'put API - usuariosPut'
+	});
+};
+
+const usuariosPatch = (req, res = response) => {
+	res.json({
+		msg: 'patch API - usuariosPatch'
+	});
+};
+const usuariosDelete = (req, res = response) => {
+	res.json({
+		msg: 'delete API - usuariosDelete'
+	});
+};
+
+module.exports = {
+	usuariosGet,
+	usuariosPost,
+	usuariosPut,
+	usuariosPatch,
+	usuariosDelete
+};
