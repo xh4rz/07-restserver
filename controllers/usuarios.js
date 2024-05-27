c54c10f6a81efa5eae87057f1d9f4035ab8e6af4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-26 23:37:38 -0500

    127. POST: Creando un usuario en la colección

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express');
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 	const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
@@ -13,13 +14,14 @@ const usuariosGet = (req = request, res = response) => {
 	});
 };
 
-const usuariosPost = (req, res = response) => {
-	const { nombre, edad } = req.body;
+const usuariosPost = async (req, res = response) => {
+	const body = req.body;
+	const usuario = new Usuario(body);
+
+	await usuario.save();
 
 	res.json({
-		msg: 'post API - usuariosPost',
-		nombre,
-		edad
+		usuario
 	});
 };
 
