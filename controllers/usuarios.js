8114fc53f0e4081792e1ede41238027e5209643e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-26 23:47:48 -0500

    128. BcryptJS - Encriptando la contraseña

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
@@ -15,9 +16,22 @@ const usuariosGet = (req = request, res = response) => {
 };
 
 const usuariosPost = async (req, res = response) => {
-	const body = req.body;
-	const usuario = new Usuario(body);
+	const { nombre, correo, password, rol } = req.body;
 
+	const usuario = new Usuario({
+		nombre,
+		correo,
+		password,
+		rol
+	});
+
+	// Verificar si el correo existe
+
+	// Encriptar la contraseña
+	const salt = bcryptjs.genSaltSync();
+	usuario.password = bcryptjs.hashSync(password, salt);
+
+	// Guardar en DB
 	await usuario.save();
 
 	res.json({
