3ee8531d293bc10e533fa3cb87bb4d980eff6531
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-28 02:33:31 -0500

    134. PUT: Actualizar información del usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -37,12 +37,23 @@ const usuariosPost = async (req, res = response) => {
 	});
 };
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async (req, res = response) => {
 	const { id } = req.params;
+
+	const { password, google, correo, ...resto } = req.body;
+
+	// todo: validar contra base de datos
+	if (password) {
+		// Encriptar la contraseña
+		const salt = bcryptjs.genSaltSync();
+		resto.password = bcryptjs.hashSync(password, salt);
+	}
+
+	const usuario = await Usuario.findByIdAndUpdate(id, resto);
 
 	res.json({
 		msg: 'put API - usuariosPut',
-		id
+		usuario
 	});
 };
 
