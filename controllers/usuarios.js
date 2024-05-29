d9cba5f7ce47062b1a890a7412417aa06824f71d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-28 22:47:03 -0500

    137. Retornar número total de registros en una colección

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,11 +5,15 @@ const Usuario = require('../models/usuario');
 const usuariosGet = async (req = request, res = response) => {
 	const { limite = 5, desde = 0 } = req.query;
 
-	const usuarios = await Usuario.find()
-		.skip(Number(desde))
-		.limit(Number(limite));
+	const query = { estado: true };
+
+	const [total, usuarios] = await Promise.all([
+		Usuario.countDocuments(query),
+		Usuario.find(query).skip(Number(desde)).limit(Number(limite))
+	]);
 
 	res.json({
+		total,
 		usuarios
 	});
 };
