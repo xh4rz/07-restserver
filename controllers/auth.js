4bad3b433c528a732b2c845d7b7ce91a23d88093
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-04 10:25:17 -0500

    149. Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,47 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const login = (req, res = response) => {
-	res.json({
-		msg: 'Login ok'
-	});
+const login = async (req, res = response) => {
+	const { correo, password } = req.body;
+
+	try {
+		// verificar si el email existe
+		const usuario = await Usuario.findOne({ correo });
+
+		if (!usuario) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - correo'
+			});
+		}
+
+		// si el usuario esta activo
+		if (!usuario.estado) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - estado: false'
+			});
+		}
+
+		// verificar la contraseña
+		const validPassword = bcryptjs.compareSync(password, usuario.password);
+
+		if (!validPassword) {
+			return res.status(400).json({
+				msg: 'Usuario / Password no son correctos - password'
+			});
+		}
+
+		// generar el JWT
+
+		res.json({
+			msg: 'Login ok'
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			msg: 'Hable con el administrador'
+		});
+	}
 };
 
 module.exports = {
