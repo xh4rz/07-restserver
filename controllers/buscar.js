10c10ded256418f00b51bd6c9411bd62f9e03cf4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-10 21:14:22 -0500

    185. Búsquedas en base de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,12 +1,46 @@
 const { response } = require('express');
+const { ObjectId } = require('mongoose').Types;
+const { Usuario, Categoria, Producto } = require('../models');
+
+const coleccionesPermitidas = ['usuarios', 'categoria', 'productos', 'roles'];
+
+const buscarUsuarios = async (termino = '', res = response) => {
+	const esMongoID = ObjectId.isValid(termino);
+
+	if (esMongoID) {
+		const usuario = await Usuario.findById(termino);
+
+		res.json({
+			results: usuario ? [usuario] : []
+		});
+	}
+};
 
 const buscar = (req, res = response) => {
 	const { coleccion, termino } = req.params;
 
-	res.json({
-		coleccion,
-		termino
-	});
+	if (!coleccionesPermitidas.includes(coleccion)) {
+		return res.status(400).json({
+			msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
+		});
+	}
+
+	switch (coleccion) {
+		case 'usuarios':
+			buscarUsuarios(termino, res);
+			break;
+
+		case 'categoria':
+			break;
+
+		case 'productos':
+			break;
+
+		default:
+			res.status(500).json({
+				msg: 'Se le olvido hacer esta busqueda'
+			});
+	}
 };
 
 module.exports = { buscar };
