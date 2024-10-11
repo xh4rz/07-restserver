a011bb4ae7d40ae7fa6c065a65de01fefa27725a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-10 20:33:54 -0500

    184. Ruta para realizar búsquedas

---
+++ controllers/buscar.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+
+const buscar = (req, res = response) => {
+	const { coleccion, termino } = req.params;
+
+	res.json({
+		coleccion,
+		termino
+	});
+};
+
+module.exports = { buscar };
--- models/server.js
+++ models/server.js
@@ -11,7 +11,8 @@ class Server {
 			auth: '/api/auth',
 			usuarios: '/api/usuarios',
 			categorias: '/api/categorias',
-			productos: '/api/productos'
+			productos: '/api/productos',
+			buscar: '/api/buscar'
 		};
 
 		// Conectar a base de datos
@@ -44,6 +45,7 @@ class Server {
 		this.app.use(this.paths.usuarios, require('../routes/usuarios'));
 		this.app.use(this.paths.categorias, require('../routes/categorias'));
 		this.app.use(this.paths.productos, require('../routes/productos'));
+		this.app.use(this.paths.buscar, require('../routes/buscar'));
 	}
 
 	listen() {
