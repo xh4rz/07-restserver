7ca94aa60a5757f4af1bebe02919c55541370f04
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-14 22:47:37 -0500

    201. Resolución de la tarea - Desestructurar de undefined

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,16 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos, validarArchivoSubir } = require('../middlewares');
 const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
 
-router.post('/', cargarArchivo);
+router.post('/', validarArchivoSubir, cargarArchivo);
 
 router.put(
 	'/:coleccion/:id',
 	[
+		validarArchivoSubir,
 		check('id', 'El id debe de ser de mongo').isMongoId(),
 		check('coleccion').custom((c) =>
 			coleccionesPermitidas(c, ['usuarios', 'productos'])
