3eb39221c39bee6193f24abbd63555ccf40d4688
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-14 22:36:50 -0500

    200. Actualizar imagen de usuario

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const { subirArchivo } = require('../helpers');
+const { Usuario, Producto } = require('../models');
 
 const cargarArchivo = async (req, res = response) => {
 	if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
@@ -20,7 +21,40 @@ const cargarArchivo = async (req, res = response) => {
 const actualizarImagen = async (req, res = response) => {
 	const { id, coleccion } = req.params;
 
-	res.json({ id, coleccion });
+	let modelo;
+
+	switch (coleccion) {
+		case 'usuarios':
+			modelo = await Usuario.findById(id);
+
+			if (!modelo) {
+				return res.status(400).json({
+					msg: `No existe un usuario con el id ${id}`
+				});
+			}
+			break;
+
+		case 'productos':
+			modelo = await Producto.findById(id);
+
+			if (!modelo) {
+				return res.status(400).json({
+					msg: `No existe un producto con el id ${id}`
+				});
+			}
+			break;
+
+		default:
+			return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+	}
+
+	const nombre = await subirArchivo(req.files, undefined, coleccion);
+
+	modelo.img = nombre;
+
+	await modelo.save();
+
+	res.json(modelo);
 };
 
 module.exports = {
