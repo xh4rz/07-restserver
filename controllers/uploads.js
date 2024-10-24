2ba1c98be18be32cf79f4acad56d3d203be3035b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-24 14:38:25 -0500

    203. Servicio para mostrar las imágenes

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -75,7 +75,60 @@ const actualizarImagen = async (req, res = response) => {
 	}
 };
 
+const mostrarImagen = async (req, res = response) => {
+	const { id, coleccion } = req.params;
+
+	let modelo;
+
+	switch (coleccion) {
+		case 'usuarios':
+			modelo = await Usuario.findById(id);
+
+			if (!modelo) {
+				return res.status(400).json({
+					msg: `No existe un usuario con el id ${id}`
+				});
+			}
+			break;
+
+		case 'productos':
+			modelo = await Producto.findById(id);
+
+			if (!modelo) {
+				return res.status(400).json({
+					msg: `No existe un producto con el id ${id}`
+				});
+			}
+			break;
+
+		default:
+			return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+	}
+
+	// limpiar imagenes previas
+
+	if (modelo.img) {
+		// borrar imagen del servidor
+
+		const pathImagen = path.join(
+			__dirname,
+			'../uploads',
+			coleccion,
+			modelo.img
+		);
+
+		if (fs.existsSync(pathImagen)) {
+			return res.sendFile(pathImagen);
+		}
+	}
+
+	res.json({
+		msg: 'falta place holder'
+	});
+};
+
 module.exports = {
 	cargarArchivo,
-	actualizarImagen
+	actualizarImagen,
+	mostrarImagen
 };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const {
+	cargarArchivo,
+	actualizarImagen,
+	mostrarImagen
+} = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
@@ -19,6 +23,18 @@ router.put(
 		validarCampos
 	],
 	actualizarImagen
+);
+
+router.get(
+	'/:coleccion/:id',
+	[
+		check('id', 'El id debe de ser de mongo').isMongoId(),
+		check('coleccion').custom((c) =>
+			coleccionesPermitidas(c, ['usuarios', 'productos'])
+		),
+		validarCampos
+	],
+	mostrarImagen
 );
 
 module.exports = router;
