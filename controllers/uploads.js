45df53ac67e4483d06158d4d307f9e065878c91e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-25 11:54:46 -0500

    207. Borrar imágenes de Cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -109,6 +109,13 @@ const actualizarImagenCloudinary = async (req, res = response) => {
 
 	// limpiar imagenes previas
 	if (modelo.img) {
+		const nombreArr = modelo.img.split('/');
+
+		const nombre = nombreArr[nombreArr.length - 1];
+
+		const [public_id] = nombre.split('.');
+
+		cloudinary.uploader.destroy(public_id);
 	}
 
 	try {
