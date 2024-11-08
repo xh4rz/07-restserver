f0a79327f7edd0c5d8a2ccce1eff49e658514fa8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-08 00:12:47 -0500

    240. Configurar Socket.io

--- models/server.js
+++ models/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT;
+		this.server = require('http').createServer(this.app);
+		this.io = require('socket.io')(this.server);
 
 		this.paths = {
 			auth: '/api/auth',
@@ -25,6 +28,9 @@ class Server {
 
 		// Rutas de mi aplicación
 		this.routes();
+
+		// Sockets
+		this.sockets();
 	}
 
 	async conectarDB() {
@@ -60,8 +66,12 @@ class Server {
 		this.app.use(this.paths.uploads, require('../routes/uploads'));
 	}
 
+	sockets() {
+		this.io.on('connection', socketController);
+	}
+
 	listen() {
-		this.app.listen(this.port, () => {
+		this.server.listen(this.port, () => {
 			console.log('Servidor corriendo en puerto', this.port);
 		});
 	}
