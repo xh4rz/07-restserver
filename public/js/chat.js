38b0ff225c22ae184cff40fe234b7ac861a68f9c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-08 02:00:09 -0500

    243. Validar Token - Frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,32 @@
-const socket = io();
+const url = 'http://localhost:8080/api/auth/';
+
+let usuario = null;
+let socket = null;
+
+// validar token de localStorage
+const validarJWT = async () => {
+	const token = localStorage.getItem('token') || '';
+
+	if (token.length <= 10) {
+		window.location = 'index.html';
+		throw new Error('No hay token en el servidor');
+	}
+
+	const resp = await fetch(url, {
+		headers: { 'x-token': token }
+	});
+
+	const { usuario: userDB, token: tokenDB } = await resp.json();
+
+	localStorage.setItem('token', tokenDB);
+
+	usuario = userDB;
+};
+
+const main = async () => {
+	await validarJWT();
+};
+
+main();
+
+// const socket = io();
