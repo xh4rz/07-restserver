08e0005510229f1d11adfd3a883c0ca7b1fade90
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-10 01:06:17 -0500

    246. Modelo para el manejo de usuarios conectados y mensajes

--- models/index.js
+++ models/index.js
@@ -3,11 +3,13 @@ const Role = require('./role');
 const Server = require('./server');
 const Usuario = require('./usuario');
 const Producto = require('./producto');
+const ChatMensajes = require('./chat-mensajes');
 
 module.exports = {
 	Categoria,
 	Role,
 	Server,
 	Usuario,
-	Producto
+	Producto,
+	ChatMensajes
 };
