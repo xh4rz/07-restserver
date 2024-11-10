226d1c923b50b69c6f8015a81df39f67e07aaabe
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-10 01:44:52 -0500

    248. Mostar en el HTML los usuarios conectados

--- public/js/chat.js
+++ public/js/chat.js
@@ -51,11 +51,26 @@ const conectarSocket = async () => {
 
 	socket.on('recibir-mensajes', () => {});
 
-	socket.on('usuarios-activos', (payload) => {
-		console.log(payload);
-	});
+	socket.on('usuarios-activos', dibujarUsuarios);
 
 	socket.on('mensaje-privado', () => {});
+};
+
+const dibujarUsuarios = (usuarios = []) => {
+	let usersHtml = '';
+
+	usuarios.forEach(({ nombre, uid }) => {
+		usersHtml += `
+		<li>
+			<p>
+				<h5 class="text-success">${nombre}</h5>
+				<span class="fs-6 text-muted">${uid}</span>
+			</p>
+		</li>
+	`;
+	});
+
+	ulUsuarios.innerHTML = usersHtml;
 };
 
 const main = async () => {
